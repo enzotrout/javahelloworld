488b7daf0bd9561e0bbb55f247fbef8311228782
enzotrout <enzotrout@gmail.com>
2015-08-20 20:37:02 +0000

    commit 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("hello world");
-		System.out.println("hello again and again and again");
+		System.out.println("hello, This is Magic! ");
 	}
 }
 	
